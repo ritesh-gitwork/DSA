ab8be3646a76f138dfe5102ce7ce8783ee951689
Ritesh <riteshmca.321@gmail.com>
2025-07-06 23:33:29 +0530

    Update fibonacci-number.cpp

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -17,3 +17,15 @@ public:
         return fib(n, memo);
     }
 };
+
+// easy but takes time
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//         if (n == 0) return 0;
+//         if (n == 1) return 1;
+
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// };
